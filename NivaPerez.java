0681b0518a2ef1c3a8e29b32284e54025535860f
Niva13 <nivaperez01@gmail.com>
2025-03-24 10:37:49 +0200

    first commit

---
+++ NivaPerez.java
@@ -0,0 +1,7 @@
+public class NivaPerez{
+        public static void main(String[] args){
+            for(int i =1;i<=5;i++){
+            System.out.println("Niva Perez.. "+i);
+        }
+    }
+}
