a6f48a0fc890a27e9615e3d258eb487971a0776c
Niva13 <nivaperez01@gmail.com>
2025-03-24 11:12:26 +0200

    second 2 commit

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello{
     {
         for(int i=1; i<=10; i++)
         {
-            System.out.println("Hello beautiful World.. "+i);
+            System.out.println("Hello very beautiful World.. "+i);
         }
     }
 }
